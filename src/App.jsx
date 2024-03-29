156a04239fd65b82f852e5bd5254ddef5725c6a7
Md Razi Ullah <mdraziullah631@gmail.com>
2024-03-29 22:18:18 +0530

    Add XWeatherApp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import XWeatherApp from './components/XWeatherApp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <XWeatherApp />
+      </div>
+    </>
+  )
+}
+
+export default App
